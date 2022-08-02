1d8e3fc4dcf5fcaa07dd5dfd23d400872bc37449
unknown <repeater1384@naver.com>
2022-08-02 23:52:48 +0900

    0802 집공부마무리

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Node {
-	int left;
-	int right;
-
-	public Node(int left, int right) {
-		this.left = left;
-		this.right = right;
-	}
-
-}
-
 public class Main {
-	static Node[] arr;
-	static ArrayList<Integer> inorder_result;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		arr = new Node[N + 1];
-		inorder_result = new ArrayList<>();
-		int answer = 0;
+		int[][] matrix = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			if (b != -1)
-				answer += 2;
-			if (c != -1)
-				answer += 2;
-			arr[a] = new Node(b, c);
-		}
-
-		inorder1(1);
-		int target = inorder_result.get(N - 1);
-		int cur_idx = 1;
-		while (true) {
-			if (cur_idx == target) {
-				System.out.println(answer);
-				break;
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
 			}
-			answer--;
-			cur_idx = arr[cur_idx].right;
-
 		}
-	}
 
-	static void inorder1(int idx) {
-		Node cur = arr[idx];
-		if (cur.left != -1)
-			inorder1(cur.left);
-		inorder_result.add(idx);
-		if (cur.right != -1)
-			inorder1(cur.right);
+		// a, i, j -> a모양으로 i,j위치에 배치 될수 있는 가짓수
+		// 0 -> 가로 1 -> 세로 2 -> 대각선
+		int[][][] dp = new int[3][N + 1][N + 1];
+		dp[0][1][2] = 1;
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 3; j <= N; j++) {
+				if (matrix[i - 1][j - 1] != 1) {
+					dp[0][i][j] = dp[0][i][j - 1] + dp[2][i][j - 1];
+					dp[1][i][j] = dp[1][i - 1][j] + dp[2][i - 1][j];
+				}
+				if (i-2>=0 && matrix[i - 1][j - 1] != 1 && matrix[i - 1][j - 2] != 1 && matrix[i - 2][j - 1] != 1)
+					dp[2][i][j] = dp[0][i - 1][j - 1] + dp[1][i - 1][j - 1] + dp[2][i - 1][j - 1];
+			}
+		}
+		System.out.println(Arrays.deepToString(dp));
+		System.out.println(dp[0][N][N] + dp[1][N][N] + dp[2][N][N]);
 	}
 
 }
