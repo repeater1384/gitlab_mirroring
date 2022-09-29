c35de716b7bbb0db3294870b58a838833a5544af
unknown <repeater1384@naver.com>
2022-09-29 18:57:51 +0900

    1

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
-import java.util.StringTokenizer;
-
-class Edge implements Comparable<Edge> {
-	int to, weight;
-
-	public Edge(int to, int weight) {
-		super();
-		this.to = to;
-		this.weight = weight;
-	}
-
-	@Override
-	public int compareTo(Edge o) {
-		return Integer.compare(this.weight, o.weight);
-	}
-
-}
-
+import java.util.Scanner;
+ 
 public class Main {
-	static int N, V, K;
-	static int[][] adjMatrix;
-	static Set<Integer>[] allPath;
-
-	public static void main(String[] args) throws IOException {
-		StringTokenizer st = null;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-
-		adjMatrix = new int[N + 1][N + 1];
-		allPath = new HashSet[N + 1];
-		for (int i = 1; i <= N; i++) {
-			allPath[i] = new HashSet<Integer>();
-		}
-
-		while (V-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			adjMatrix[a][b] = cost;
-		}
-
-		dijkstra(1);
-		for (int i = 1; i <= N; i++) {
-			if (allPath[i].size() < K)
-				System.out.println(-1);
-			else {
-				Integer[] temp = allPath[i].toArray(new Integer[0]);
-				Arrays.sort(temp);
-				System.out.println(temp[K - 1]);
-			}
-		}
-
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(recursive(N,0));
+		sc.close();
 	}
 
-	static void dijkstra(int start) {
-
-		PriorityQueue<Edge> pq = new PriorityQueue<>();
-
-		pq.add(new Edge(start, 0));
-
-		while (!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			allPath[cur.to].add(cur.weight);
-			
-			for (int i = 1; i <= N; i++) {
-				if(adjMatrix[cur.to][i]==0)continue;
-				if(allPath[i].size()>K)continue;
-				int cost = cur.weight + adjMatrix[cur.to][i];
-				pq.add(new Edge(i, cost));
-			}
-		}
-	}
+	static int recursive(int cur, int count){
+		if(cur<=1)
+			return count;
+		return Math.min(recursive(cur/2, count+1+(cur%2)), recursive(cur/3, count+1+(cur%3)));
+	}	
 }
