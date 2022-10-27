e2cb0dc35349a0b2fa4aeb85dc86279b040fd882
unknown <repeater1384@naver.com>
2022-10-27 23:44:40 +0900

    bellman-ford

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+	static int N, M;
+	static List<List<int[]>> adjList;
+	static long[] dist;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-		int[] memory = new int[N];
-		int[] needResource = new int[N];
-		for (int i = 0; i < N; i++)
-			memory[i] = sc.nextInt();
-		for (int i = 0; i < N; i++)
-			needResource[i] = sc.nextInt();
-		int[] dp = new int[M+1];
-		
-		Arrays.fill(dp, Integer.MAX_VALUE);
-		for(int i = 0 ; i<N;i++) {
-			int[] temp = dp.clone();
-			for(int m = 0 ; m<=M;m++) {
-				if(memory[i] >= m) {
-					temp[m] = Math.min(temp[m], needResource[i]);
-				}else if (dp[m-memory[i]] != Integer.MAX_VALUE) {
-					temp[m] = Math.min(temp[m], temp[m-memory[i]]+needResource[i]);
-				}
-			}
-			if(i<=3)
-				System.out.println(Arrays.toString(temp));
-			dp = temp;
+		adjList = new ArrayList<>();
+		for (int i = 0; i < N + 1; i++) {
+			adjList.add(new ArrayList<>());
 		}
-		System.out.println(dp[M]);
-		sc.close();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+			adjList.get(A).add(new int[] { B, C });
+		}
+
+		dist = new long[N + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		boolean isNegative = bellman_ford(1);
+		if (isNegative) {
+			System.out.println(-1);
+		} else {
+			for (int i = 2; i < N + 1; i++)
+				System.out.println(dist[i] == Integer.MAX_VALUE ? -1 : dist[i]);
+		}
 	}
 
+	static boolean bellman_ford(int start) {
+		dist[start] = 0;
+
+		// N-1 번 반복
+		int loop = N - 1;
+		while (loop-- > 0) {
+			for (int i = 1; i < N + 1; i++) {
+				if (dist[i] == Integer.MAX_VALUE)
+					continue;
+
+				for (int[] edge : adjList.get(i)) {
+					int j = edge[0];
+					int cost = edge[1];
+					dist[j] = Math.min(dist[j], dist[i] + cost);
+				}
+			}
+		}
+
+		// N번째 반복에서 갱신되면 음의 사이클 존재
+		for (int i = 1; i < N + 1; i++) {
+			if (dist[i] == Integer.MAX_VALUE)
+				continue;
+
+			for (int[] edge : adjList.get(i)) {
+				int j = edge[0];
+				int cost = edge[1];
+				if (dist[j] > dist[i] + cost)
+					return true;
+			}
+		}
+		return false;
+	}
 }
